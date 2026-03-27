ea1bee3a2189ac5658631ee30443937cfa5ba4f9
Beronica <beronicabayot28@gmail.com>
2026-03-27 15:43:45 +0800

    commit

---
+++ BaonTrackerPro/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace BaonTrackerPro.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
