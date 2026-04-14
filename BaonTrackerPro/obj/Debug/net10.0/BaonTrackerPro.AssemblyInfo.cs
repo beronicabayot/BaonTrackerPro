66a45f51379075687fe30b386ae6afae27999b4b
XavierFD64 <beronicabayot28@gmail.com>
2026-04-14 23:26:27 +0800

    daw4

--- BaonTrackerPro/obj/Debug/net10.0/BaonTrackerPro.AssemblyInfo.cs
+++ BaonTrackerPro/obj/Debug/net10.0/BaonTrackerPro.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BaonTrackerPro")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c95732636d1eacc43485e9b4039209181e954a1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58d2f5bd8139715a00db4e375ca59eeb9d543ad5")]
 [assembly: System.Reflection.AssemblyProductAttribute("BaonTrackerPro")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BaonTrackerPro")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
